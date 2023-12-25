7d84cfd728212e4125705d11d0d5abb58a2207ef
Felicia Atterling <felicia.atterling@gmail.com>
2023-12-25 15:25:42 +0100

    set up

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Färgmagasinet</title>
+            </Head>
+            <Header></Header>
+            <body>
+                <h1>Hello</h1>
+            </body>
+        </>
+        
+    )
+}
